baab0a5f07604de0cd7ce309db146d1bbd900c89
skeks <azarov990@gmail.com>
2026-06-29 03:01:05 +0300

    relise 1.1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+int addition(int x, int y)
+{
+
+	return x + y;
+}
+int subtraction(int x, int y)
+{
+	return x - y;
+}
+int multiplication(int x, int y)
+{
+	return x * y;
+}
+int division(int x, int y)
+{
+	if (y != 0)
+	{
+		return x / y;
+	}else{
+		return 0;
+	}
+	
+}
+void printResult(int a, int b, int c, char operation)
+{
+	if (operation == '/' && b == 0)
+	{	
+		std::cout << "devision by zero" << std::endl;
+		
+	}
+	else
+	{
+		std::cout << a << ' ' << operation << ' ' << b << " = " << c << '\n';
+	}
+}
+
 int main()
 {
 	std::cout << "Hello!" << std::endl;
@@ -10,40 +46,37 @@ int main()
 	char op;
 	while (true)
 	{
-		std::cout << "choose an operation (+ - * /): ";
+		std::cout << "choose an operation (+ - * / q): ";
 		std::cin >> op;
 
 		if (op == 'q')
 		{
-			std::cout << "goodbye" << std::endl;
+			std::cout << "goodbye!" << std::endl;
 			break;
 		}
 
 		std::cout << "enter 2 numbers: ";
 		std::cin >> x;
 		std::cin >> y;
-
-		
 		if (op == '+')
 		{
-			std::cout << "result: " << x + y << std::endl;
+			c = addition(x, y);
+			printResult(x, y, c, op);
 		}
 		if (op == '-')
 		{
-			std::cout << "result: " << x - y << std::endl;
+			c = subtraction(x, y);
+			printResult(x, y, c, op);
 		}
 		if (op == '*')
 		{
-			std::cout << "result: " << x * y << std::endl;
+			c = multiplication(x, y);
+			printResult(x, y, c, op);
 		}
-		if (op == '/' && y != 0)
+		if (op == '/')
 		{
-			std::cout << "result: " << x / y << std::endl;
-		}
-		else
-		{
-			std::cout << "division by zero!!!" << std::endl;
-			continue;
+			c = division(x, y);
+			printResult(x, y, c, op);
 		}
 	}
 
